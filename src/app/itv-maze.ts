5c84a4aa4457e72b6d8d9e9fe2bbc545f2587e04
Vaishnavi-CR <59356973+Vaishnavi-CR@users.noreply.github.com>
2020-02-11 23:24:33 -0800

    component creation and UI with dummy data

---
+++ src/app/itv-maze.ts
@@ -0,0 +1,12 @@
+export interface ITvMaze {
+id: number
+name: string,
+language: string,
+genre: string[],
+runtime: number,
+time: string,
+days: string[],
+nwname: string,
+timezone: string,
+image: string,
+}
